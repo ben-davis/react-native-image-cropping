3d17e32db2c42c6928fc858bdf854c64e9be228e
Ben Davis <ben@yplanapp.com>
2016-06-03 09:50:08 +0100

    Added 16x9

--- ReactNativeImageCropping.m
+++ ReactNativeImageCropping.m
@@ -20,8 +20,8 @@ RCT_ENUM_CONVERTER(TOCropViewControllerAspectRatio, (@{
                  @"AspectRatio4x3" : @(TOCropViewControllerAspectRatio4x3),
                  @"AspectRatio5x4" : @(TOCropViewControllerAspectRatio5x4),
                  @"AspectRatio7x5" : @(TOCropViewControllerAspectRatio7x5),
-                @"AspectRatio9x16" : @(TOCropViewControllerAspectRatio9x16)
-                @"AspectRatio16x9" : @(TOCropViewControllerAspectRatio16x9)
+                @"AspectRatio9x16" : @(TOCropViewControllerAspectRatio9x16),
+                @"AspectRatio16x9" : @(TOCropViewControllerAspectRatio16x9),
                 }), UIStatusBarAnimationNone, integerValue)
 @end
 
